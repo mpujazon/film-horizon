045400ac8f3eaeefc2c3556ff80fd1ca3306d0ac
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-03-12 10:42:27 +0100

    NavBar Component implemented

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './navbar.html'
 })
 export class Navbar {
-
+  appName = 'FilmHorizon';
+  links = [
+    {
+      id:1,
+      label:'Home',
+      path:'/'
+    },
+    {
+      id:2,
+      label:'Movies',
+      path:'/movies'
+    },
+    {
+      id:3,
+      label:'TV Shows',
+      path:'/tv-shows'
+    }
+  ];
 }
