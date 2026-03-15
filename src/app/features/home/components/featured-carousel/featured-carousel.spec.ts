30ce2d6d4cae09c93c7087d2326b69547a2c5a19
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-03-15 23:04:03 +0100

    FeaturedCarousel Component Implemented

--- src/app/features/home/components/featured-carousel/featured-carousel.spec.ts
+++ src/app/features/home/components/featured-carousel/featured-carousel.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 
 import { FeaturedCarousel } from './featured-carousel';
 
@@ -8,13 +9,17 @@ describe('FeaturedCarousel', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FeaturedCarousel]
+      imports: [FeaturedCarousel],
+      providers: [provideRouter([])]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(FeaturedCarousel);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.componentRef.setInput('title', 'The Last Horizon');
+    fixture.componentRef.setInput('description', 'A pilot chases a signal beyond known space.');
+    fixture.componentRef.setInput('imageUrl', 'https://image.tmdb.org/t/p/original/backdrop.jpg');
+    fixture.detectChanges();
   });
 
   it('should create', () => {
--- src/app/features/home/components/featured-carousel/featured-carousel.ts
+++ src/app/features/home/components/featured-carousel/featured-carousel.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input, output } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-featured-carousel',
-  imports: [],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './featured-carousel.html'
 })
 export class FeaturedCarousel {
+  readonly title = input.required<string>();
+  readonly description = input.required<string>();
+  readonly imageUrl = input.required<string>();
+  readonly movieId = input<number>(1);
 
+  readonly watchlistClicked = output<void>();
+
+  protected readonly movieDetailLink = computed(() => `/movie/${this.movieId()}`);
+
+  protected onAddToWatchlist(): void {
+    this.watchlistClicked.emit();
+  }
 }
