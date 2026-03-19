713e5499d9564e29abecf836e2ca7dec9e955ab4
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-03-19 09:15:58 +0100

    Actors view implented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import {Homepage} from './features/home/pages/homepage/homepage';
 import { SearchResults } from './features/search/pages/search-results/search-results';
 import { MediaDetailPage } from './features/details/pages/media-detail/media-detail';
 import { mediaDetailResolver } from './features/details/resolvers/media-detail.resolver';
+import { ActorDetailPage } from './features/details/pages/actor-detail/actor-detail';
+import { actorDetailResolver } from './features/details/resolvers/actor-detail.resolver';
 
 export const routes: Routes = [
   {
@@ -25,6 +27,13 @@ export const routes: Routes = [
     component: MediaDetailPage,
     resolve: {
       media: mediaDetailResolver
+    }
+  },
+  {
+    path: 'actor/:id',
+    component: ActorDetailPage,
+    resolve: {
+      actor: actorDetailResolver
     }
   },
   {
