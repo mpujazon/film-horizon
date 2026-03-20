304b0ab76fea1a81177619cc3e569039c8394941
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-03-20 14:42:04 +0100

    Log In Validation Implemented

--- src/app/features/auth/pages/login/login.ts
+++ src/app/features/auth/pages/login/login.ts
@@ -1,29 +1,53 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth/auth-service';
-import {FormsModule} from '@angular/forms';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Router, RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   imports: [
-    FormsModule,
-    RouterLink
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './login.html'
 })
 export class Login {
   private readonly authService = inject(AuthService);
   private readonly router = inject(Router);
+  readonly formBuilder = inject(FormBuilder);
 
-  email = '';
-  password = '';
+  loginForm = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.minLength(8), Validators.required]]
+  });
+
+  isSubmitting = false;
 
   async onSubmit(){
+    this.isSubmitting = true;
     try{
-      await this.authService.login(this.email, this.password);
+      const {email, password} = this.loginForm.getRawValue();
+      await this.authService.login(email,password);
       await this.router.navigate(['/']);
     }catch(error){
       console.error('Error on login', error);
+    }finally {
+      this.isSubmitting = false;
     }
+  }
+
+  showError(controlName: 'email' | 'password'): boolean {
+    const c = this.loginForm.get(controlName);
+    return !!c && c.invalid && (c.dirty || c.touched);
+  }
+
+  getError(controlName: 'email' | 'password'): string | null {
+    const c = this.loginForm.get(controlName);
+    if (!c?.errors) return null;
+
+    if (c.errors['required']) return 'Required';
+    if (c.errors['email']) return 'Email inalid';
+    if (c.errors['minlength']) return `At least ${c.errors['minlength'].requiredLength} characters`;
+    return 'Value not valid';
   }
 }
