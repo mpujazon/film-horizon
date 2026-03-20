1c472e3d9e7223210414cf3924fba9b77c5d9b4a
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-03-20 12:28:22 +0100

    AuthService Implemented

--- src/app/core/services/auth/auth-service.ts
+++ src/app/core/services/auth/auth-service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Auth, authState, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, User } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  
+  private readonly auth: Auth = inject(Auth);
+
+  readonly user$: Observable<User | null> = authState(this.auth);
+
+  register(email: string, password: string){
+    return createUserWithEmailAndPassword(this.auth, email, password);
+  }
+
+  login(email: string, password: string){
+    return signInWithEmailAndPassword(this.auth, email, password);
+  }
+
+  logout() {
+    return signOut(this.auth);
+  }
 }
