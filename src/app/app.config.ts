b726063c9c52693958e0167dac22e4f77295629c
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-03-20 11:24:35 +0100

    Firebase Configuration Added and configured in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { environment } from '@environments/environment';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
   ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,14 @@ export const environment = {
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/',
   tmdbApiKey:
-    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMGFjZGE0Y2E4NWQzOWFmMTc1ZjA1ZTEzM2Y5NGJmNiIsIm5iZiI6MTc3MzIzMTkxOS41MTksInN1YiI6IjY5YjE1ZjJmY2Q4MjIxMGFmZDFiN2UzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GWtgZ_HCwIQgRqFx46XlEU2hHW9DM7Y_vuFAiIOfyY8'
+    'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMGFjZGE0Y2E4NWQzOWFmMTc1ZjA1ZTEzM2Y5NGJmNiIsIm5iZiI6MTc3MzIzMTkxOS41MTksInN1YiI6IjY5YjE1ZjJmY2Q4MjIxMGFmZDFiN2UzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.GWtgZ_HCwIQgRqFx46XlEU2hHW9DM7Y_vuFAiIOfyY8',
+  firebaseConfig: {
+    apiKey: "AIzaSyBhvMRe7tUl2m-FzBJ0OnhBTFX-Obg6ym4",
+    authDomain: "filmhorizon-68063.firebaseapp.com",
+    projectId: "filmhorizon-68063",
+    storageBucket: "filmhorizon-68063.firebasestorage.app",
+    messagingSenderId: "1036191155736",
+    appId: "1:1036191155736:web:02ec18e8520c11a43e656d",
+    measurementId: "G-BSX1PZWWF2"
+  }
 };
