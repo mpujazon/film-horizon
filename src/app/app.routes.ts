b2637aa495ffc3e4a39ede0e7ea775a4524f1ce9
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-03-20 15:46:27 +0100

    Register Component implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,16 @@ import { ActorDetailPage } from './features/details/pages/actor-detail/actor-det
 import { actorDetailResolver } from './features/details/resolvers/actor-detail.resolver';
 import {Login} from './features/auth/pages/login/login';
 import {authGuard} from './core/guards/auth-guard';
+import {Register} from './features/auth/pages/register/register/register';
 
 export const routes: Routes = [
   {
     path:'login',
     component: Login
+  },
+  {
+    path:'register',
+    component: Register
   },
   {
     path:"",
