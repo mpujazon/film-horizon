09e88bb76bb6842f54205068d8313ce5f1b288fe
Miguel Pujazón <mpujazoncardenas@gmail.com>
2026-03-29 11:56:44 +0200

    New environment template created for production

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: '__FIREBASE_API_KEY__',
+    authDomain: '__FIREBASE_AUTH_DOMAIN__',
+    projectId: '__FIREBASE_PROJECT_ID__',
+    storageBucket: '__FIREBASE_STORAGE_BUCKET__',
+    messagingSenderId: '__FIREBASE_MESSAGING_SENDER_ID__',
+    appId: '__FIREBASE_APP_ID__',
+  },
+  tmdbApiKey: '__TMDB_API_KEY__'
+};
